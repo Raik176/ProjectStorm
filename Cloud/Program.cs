19a9e89f411d4c23980aa16dcd79aff141ca1505
Raik176 <raikmaus321@gmail.com>
2024-04-21 12:26:15 +0200

    Add project files.

---
+++ Cloud/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CloudLauncher {
+    static class Program
+    {
+        public static string dll = "Rain.dll";
+        public static string launcher = "FortniteLauncher.exe";
+        public static string client = "FortniteClient-Win64-Shipping";
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Cloud/files/EpicGames.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+using static System.Environment;
+
+namespace CloudLauncher {
+    static class EpicGames
+    {
+        public class Installed
+        {
+            public class Installation
+            {
+                [JsonProperty("AppName")]
+                public string AppName { get; set; }
+
+                [JsonProperty("AppVersion")]
+                public string AppVersion { get; set; }
+            }
+        }
+
+        public static Installed LauncherInstalled => GetLauncherInstalled();
+
+        static Installed GetLauncherInstalled()
+        {
+            var path = Path.Combine(GetFolderPath(SpecialFolder.CommonApplicationData),
+                "Epic\\UnrealEngineLauncher\\LauncherInstalled.dat");
+
+            if (!File.Exists(path))
+                return new Installed();
+            else
+                return JsonConvert.DeserializeObject<Installed>(File.ReadAllText(path));
+        }
+    }
+}
